a919bdfe5b326eb80a0b690d6913cbce793a7aec
CD5620 <charlie7@illinois.edu>
2021-09-25 05:37:53 -0500

    made models

--- PygChores/PygChores/settings.py
+++ PygChores/PygChores/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'login.apps.LoginConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'schedule',
 ]
 
 MIDDLEWARE = [
--- PygChores/home/models.py
+++ PygChores/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    duration = models.IntegerField()
+    frequency = models.IntegerField()
+
+class FreeTime(models.Model):
+    start_time = models.DateTimeField('start_time')
+    end_time = models.DateTimeField('end-time')
