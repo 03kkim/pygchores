59dc0fb903b9e2fb26f6ef12730234e4694f4361
CD5620 <charlie7@illinois.edu>
2021-09-25 02:53:34 -0500

    chain of pages to landing

--- PygChores/login/urls.py
+++ PygChores/login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name='login'
 urlpatterns = [
     path('', views.main, name='main'),
+    path('redirect/', views.redir, name='redirect'),
 ]
--- PygChores/login/views.py
+++ PygChores/login/views.py
@@ -4,4 +4,8 @@ from django.template import loader
 
 # Create your views here.
 def main(request):
+    return render(request, 'login/index.html')
+    #return redirect('/accounts/login')
+
+def redir(request):
     return redirect('/accounts/login')
