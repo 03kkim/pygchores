675c462fa39ea7d9845f90422319f80892ced716
CD5620 <charlie7@illinois.edu>
2021-09-25 01:22:55 -0500

    landing page

--- PygChores/landing/views.py
+++ PygChores/landing/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
 
+# Create your views here.
+def main(request):
+    return render(request, 'landing/test.html')
 # Create your views here.
